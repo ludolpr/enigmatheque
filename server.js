714d774c6ddbc23c75d9861419f2cceabdd8dffc
ludolpr <ludolpr@gmail.com>
2022-06-29 16:56:22 +0200

    soir 29/06

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const {
     engine
 } = require('express-handlebars');
 const app = express();
-
+const port = 1990
 
 // config handlebars
 app.engine('.hbs', engine({
@@ -32,51 +32,4 @@ app.get('/proposer', function (req,res) {
 
 
 // run server
-app.listen(1990);
-
-
-
-// En === Fr
-// breaking change ====	changement incompatible
-// bringing ES6 to the Node Community! === ES6 pour la communauté Node!
-// bug === bogue
-// changelog ===	journal des modifications
-// debugging === débogage
-// drop-in === replacement	version alternative
-// event loop ===	boucle événementielle
-// handler ===	gestionnaire
-// icu ===	icu
-// memory footprint	=== occupation mémoire
-// open governance model ===	modèle de gouvernance ouvert
-// package ===	paquets
-// patch === correctif
-// roadmap === feuille de route
-// slide decks ===	présentations
-// standards === normes
-// streams	flux (streams) *(ajouter le mot anglais également)
-// socket ===	connecteur
-// tracing ===	traçage
-// working group ===	groupe de travail
-// assertion failure ===	échec d'affirmation
-
-
-
-// Note de configuration possible   
-
-// var http = require('http');
- 
-// var serv = http.createServer( //création d'un serveur web
- 
-//   function (req, res) { //callback sur les requêtes HTTP
-//       //construction d'une réponse HTTP
-//       res.writeHead(200, {'Content-Type': 'text/plain'});
-//       res.write('Hello world !');
- 
-//       res.end(); //envoi de la réponse
-//   }
- 
-// );
- 
-// serv.listen(8000); //commence à accepter les requêtes
- 
-// console.log("Server running at http://localhost:8000");
+-app.listen (port, () => console.log (`Exemple d'application écoutant sur le port ${port} !`))
