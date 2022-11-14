66b6d67036d07808fa3a8b00505214b3140175e8
ludolpr <ludolpr@gmail.com>
2022-11-14 16:28:51 +0100

    tout est ok

--- back/controllers/homeControler.js
+++ back/controllers/homeControler.js
@@ -2,7 +2,8 @@ const getPageHome = async (req, res) => {
   const dbEnigmes = await db.query(
     // ORDER BY id_enigme DESC LIMIT 3 => me prend les trois derniere enigme poster pour les afficher
     `SELECT * FROM enigme WHERE is_Verified=1 ORDER BY id_enigme DESC LIMIT 3`
-  );
+    );
+    console.log(dbEnigmes);
   res.render("home", {
     enigmes: dbEnigmes,
   });
