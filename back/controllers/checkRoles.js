4d97366d9b2865c567fb1218db8aebe48911cc59
ludolpr <ludolpr@gmail.com>
2022-11-14 11:54:23 +0100

    tout est ok

--- back/controllers/checkRoles.js
+++
@@ -1,8 +0,0 @@
-exports.checkRoles =  async {
-    const { id } = req.params;
-    const {isAdmin, isBan} = req.body
-    let [dbMembres] =  await db.query(`SELECT isAdmin,isBan FROM membres WHERE id=${id}`)
-    (dbMembres.isAdmin === "1" &&  )
-    if (vrf3{{ this.id }}.toString() === "1" ) is_Admin{{ this.id }}.checked = true;
-    user.isAdmin === req.session.user.isAdmin && user.isAdmin === 0 ) ? res.redirect('/') : next();
-}
